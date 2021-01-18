c5fdae0bee4a1f9051678de3855eea40094e90cb
= <alex.a4.25@yandex.ru>
2021-01-18 08:55:11 +0300

    Update ground asset, add shape

--- lib/components/ground.dart
+++ lib/components/ground.dart
@@ -1,46 +1,48 @@
-import 'dart:ui';
-
+import 'package:flame/extensions/vector2.dart';
+import 'package:flame/sprite.dart';
+import 'package:flame_forge2d/sprite_body_component.dart';
 import 'package:forge2d/forge2d.dart';
-import 'package:flame_forge2d/body_component.dart';
 import 'package:flame_forge2d/viewport.dart';
 
-List<Ground> createBoundaries(Viewport viewport) {
-  final Vector2 screenSize = viewport.size / viewport.scale;
-  final Vector2 bottomRight = screenSize / 2;
-  final Vector2 bottomLeft = Vector2(-bottomRight.x, bottomRight.y);
+List<Ground> createBoundaries(Viewport viewport, Sprite sprite) {
+  final size = viewport.size / viewport.scale;
+  final y = (-size.y / 2) + (sprite.srcSize.y / 2);
+  final list = <Ground>[];
+  for (int i = -2; i < 3; i++) {
+    list.add(
+      Ground(Vector2((i * sprite.image.width).toDouble(), y), sprite),
+    );
+  }
 
-  final rem = Vector2(0, 5);
-  return [
-    Ground(bottomRight - rem, bottomLeft - rem),
-  ];
+  return list;
 }
 
-class Ground extends BodyComponent {
+class Ground extends SpriteBodyComponent {
   final Vector2 start;
-  final Vector2 end;
 
-  Ground(this.start, this.end)
-      : super(
-          paint: Paint()
-            ..color = Color(0xFFFF0000)
-            ..strokeWidth = 5,
-        );
+  Ground(this.start, Sprite sprite) : super(sprite, Vector2(32, 8));
 
   @override
   Body createBody() {
-    final EdgeShape shape = EdgeShape();
-    shape.set(start, end);
+    final shape = PolygonShape();
+    final vertices = [
+      Vector2(-size.x, size.y) / 2,
+      Vector2(size.x, size.y) / 2,
+      Vector2(size.x, -size.y) / 2,
+      Vector2(-size.x, -size.y) / 2,
+    ];
 
-    final fixtureDef = FixtureDef()
+    shape.set(vertices, vertices.length);
+
+    final def = FixtureDef()
       ..shape = shape
       ..restitution = 0.0
       ..friction = 0.1;
-
     final bodyDef = BodyDef()
       ..userData = this // To be able to determine object in collision
-      ..position = Vector2.zero()
+      ..position = start
       ..type = BodyType.STATIC;
 
-    return world.createBody(bodyDef)..createFixture(fixtureDef);
+    return world.createBody(bodyDef)..createFixture(def);
   }
 }
