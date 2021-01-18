7c7dbfc252fb9b5d9568940d135290e0ad29fb34
= <alex.a4.25@yandex.ru>
2021-01-18 09:28:51 +0300

    Add joystick

--- lib/game/game.dart
+++ lib/game/game.dart
@@ -1,37 +1,66 @@
+import 'package:flame/components/joystick/joystick_action.dart';
+import 'package:flame/components/joystick/joystick_component.dart';
 import 'package:flame/extensions/vector2.dart';
 import 'package:flame/extensions/offset.dart';
 import 'package:flame/gestures.dart';
 import 'package:flame/sprite.dart';
 import 'package:flame/spritesheet.dart';
 import 'package:flame_forge2d/forge2d_game.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:game_testing/components/bullet_component.dart';
 import 'package:game_testing/components/ground.dart';
 import 'package:game_testing/components/lama_component.dart';
+import 'package:game_testing/contacts/lama_bullet_contact.dart';
 
-class LamaGame extends Forge2DGame with TapDetector {
+class LamaGame extends Forge2DGame with MultiTouchDragDetector {
+  JoystickComponent joystick;
   LamaComponent _lama;
 
-  LamaGame() : super(gravity: Vector2(.0, -10.0), scale: 4.0) {
-    addAll(createBoundaries(viewport));
+  LamaGame() : super(gravity: Vector2(.0, -10.0), scale: 2.0) {
+    addContactCallback(LamaBulletContact());
+    add(createJoystick());
+  }
+
+  JoystickComponent createJoystick() {
+    joystick = JoystickComponent(actions: [
+      JoystickAction(actionId: 1, align: JoystickActionAlign.TOP_LEFT),
+      JoystickAction(
+        actionId: 2,
+        align: JoystickActionAlign.TOP_LEFT,
+        margin: EdgeInsets.only(left: 50),
+      ),
+    ]);
+    return joystick;
   }
 
   @override
   Future<void> onLoad() async {
     await images.load('lama.png');
     await images.load('bullet.png');
+    await images.load('ground.png');
     _lama = LamaComponent(
       SpriteSheet(
-          image: images.fromCache('lama.png'), srcSize: Vector2.all(48)),
-      Vector2(0,0),
+          image: images.fromCache('lama.png'), srcSize: Vector2(24, 36)),
+      Vector2(0, 0),
     );
+    joystick.addObserver(_lama);
+    addAll(createBoundaries(
+      viewport,
+      Sprite(images.fromCache('ground.png'), srcSize: Vector2(32, 8)),
+    ));
     add(_lama);
   }
 
   @override
-  void onTapUp(v) {
-    final bullet = BulletComponent(
-        Sprite(images.fromCache('bullet.png')), v.localPosition.toVector2());
-    add(bullet);
-    _lama.jump();
+  void onReceiveDrag(DragEvent drag) {
+    joystick.onReceiveDrag(drag);
+    if (joystick.actions.every((b) => !b.isPressed)) {
+      final bullet = BulletComponent(
+        Sprite(images.fromCache('bullet.png')),
+        drag.initialPosition.toVector2(),
+      );
+      add(bullet);
+    }
+    super.onReceiveDrag(drag);
   }
 }
