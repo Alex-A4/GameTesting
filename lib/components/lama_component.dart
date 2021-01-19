ebbbb813bf4477314a67c1c038c0cc28fc51f312
= <alex.a4.25@yandex.ru>
2021-01-19 13:05:14 +0300

    Update lama's dead physic

--- lib/components/lama_component.dart
+++ lib/components/lama_component.dart
@@ -12,14 +12,31 @@ import 'package:vector_math/vector_math_64.dart';
 class LamaComponent extends SpriteAnimationBodyComponent with JoystickListener {
   final Vector2 startPosition;
   bool isJumping = false;
+
   bool isDead = false;
   bool applyDeadReaction = false;
+
+  final double originalHealth;
+  double health;
 
   /// Direction of jumping: 1 - right, -1 - left
   int jumpDirection = 1;
 
-  LamaComponent(SpriteSheet sheet, this.startPosition)
-      : super.rest(sheet, Vector2(24, 36));
+  LamaComponent(
+    SpriteSheet sheet,
+    this.startPosition, {
+    this.originalHealth = 100.0,
+  }) : super.rest(sheet, Vector2(24, 36)) {
+    this.health = originalHealth;
+  }
+
+  /// Creates a edge shape (left or right side) depends on direction.
+  /// This helps with avoiding contacts between bullet and lama when it's dead.
+  Shape get deadShape {
+    final left = Vector2(-size.x * (-jumpDirection), size.y) / 2;
+    final right = Vector2(-size.x * (-jumpDirection), -size.y) / 2;
+    return EdgeShape()..set(left, right);
+  }
 
   @override
   Body createBody() {
@@ -35,8 +52,8 @@ class LamaComponent extends SpriteAnimationBodyComponent with JoystickListener {
     final fix = FixtureDef()
       ..shape = shape
       ..restitution = 0.0
-      ..density = 1.0
-      ..friction = 0.2;
+      ..density = 0.0
+      ..friction = 1.0;
 
     final def = BodyDef()
       ..userData = this
@@ -44,7 +61,7 @@ class LamaComponent extends SpriteAnimationBodyComponent with JoystickListener {
       ..type = BodyType.DYNAMIC;
     return world.createBody(def)
       ..createFixture(fix)
-      ..setMassData(MassData()..mass = 10.0);
+      ..setMassData(MassData()..mass = 0.8);
   }
 
   @override
@@ -53,23 +70,46 @@ class LamaComponent extends SpriteAnimationBodyComponent with JoystickListener {
       isJumping = false;
       if (applyDeadReaction) {
         applyDeadReaction = false;
+        while (body.fixtures.isNotEmpty) {
+          body.destroyFixture(body.fixtures.first);
+        }
+        body.createFixture(FixtureDef()..shape = deadShape);
         body.setTransform(body.position, -1 * jumpDirection * math.pi / 2);
         body.applyLinearImpulse(Vector2(0, 30));
-      } else {
-        if (body.angularVelocity == 0.0 &&
-            body.linearVelocity == Vector2.zero()) {
-          body.setType(BodyType.KINEMATIC);
-        }
       }
     }
+    if (isDead && body.linearVelocity == Vector2.zero()) {
+      body.setType(BodyType.KINEMATIC);
+    }
 
-    if (isJumping) {
-      body.setTransform(
-        Vector2(body.position.x + jumpDirection * 30.0 * dt, body.position.y),
-        0,
-      );
+    /// Allow jump only when lama stay on ground
+    if (isJumping && body.linearVelocity == Vector2.zero()) {
+      isJumping = false;
     }
     super.update(dt);
+  }
+
+  @override
+  void render(Canvas c) {
+    super.render(c);
+    if (isDead) return;
+
+    final padding = Vector2(3 * viewport.scale, 0);
+    final origSize = size * viewport.scale - (padding * 2);
+
+    final healthPaint = Paint()
+      ..color = Color(0xFF00AF00)
+      ..strokeWidth = 2;
+    final lessHealthPaint = Paint()
+      ..color = Color(0xFF9F9F9F)
+      ..strokeWidth = 2;
+
+    final part = health / originalHealth;
+    final partPoint = Vector2(origSize.x * part, 0) + padding;
+
+    c.drawLine(padding.toOffset(), partPoint.toOffset(), healthPaint);
+    c.drawLine(partPoint.toOffset(),
+        Vector2(origSize.x + padding.x, 0).toOffset(), lessHealthPaint);
   }
 
   void jump() {
@@ -78,9 +118,8 @@ class LamaComponent extends SpriteAnimationBodyComponent with JoystickListener {
       isJumping = true;
       startAnimation(
         jumpDirection == 1 ? 0 : 1,
-        Duration(milliseconds: 50),
+        Duration(milliseconds: 70),
         loop: false,
-        completeCallback: () => isJumping = false,
       );
     }
   }
@@ -91,6 +130,7 @@ class LamaComponent extends SpriteAnimationBodyComponent with JoystickListener {
 
   @override
   void joystickAction(JoystickActionEvent event) {
+    if (isJumping || isDead) return;
     if (event.id == 1) {
       jumpDirection = -1;
       jump();
@@ -98,12 +138,23 @@ class LamaComponent extends SpriteAnimationBodyComponent with JoystickListener {
       jumpDirection = 1;
       jump();
     }
+    if (event.id == 1 || event.id == 2) {
+      body.applyLinearImpulse(Vector2(10.0 * jumpDirection, 8));
+    }
   }
 
   @override
   void joystickChangeDirectional(JoystickDirectionalEvent event) {}
 
-  void killLama() {
+  void damageLama(double damage) {
+    health -= damage;
+    if (health <= 0.0) {
+      health = 0.0;
+      _killLama();
+    }
+  }
+
+  void _killLama() {
     isDead = true;
     applyDeadReaction = true;
     setOverridePaint(Paint()..color = Color(0x5FFFFFFF));
