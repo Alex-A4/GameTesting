49a10899730a45ea3669a2212654d807f5a099e5
= <alex.a4.25@yandex.ru>
2021-01-22 13:35:18 +0300

    Update game mechanic. Auto-create lamas. Bullet delay

--- lib/game/game.dart
+++ lib/game/game.dart
@@ -1,10 +1,9 @@
-import 'package:flame/components/joystick/joystick_action.dart';
-import 'package:flame/components/joystick/joystick_component.dart';
 import 'package:flame/extensions/vector2.dart';
 import 'package:flame/extensions/offset.dart';
 import 'package:flame/gestures.dart';
 import 'package:flame/sprite.dart';
 import 'package:flame/spritesheet.dart';
+import 'package:flame/timer.dart';
 import 'package:flame_forge2d/forge2d_game.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -13,29 +12,19 @@ import 'package:game_testing/components/ground.dart';
 import 'package:game_testing/components/lama_component.dart';
 import 'package:game_testing/contacts/lama_bullet_contact.dart';
 
-class LamaGame extends Forge2DGame with MultiTouchDragDetector {
-  JoystickComponent joystick;
-  LamaComponent _lama;
+class LamaGame extends Forge2DGame with PanDetector {
+  final Duration spamSpeed;
+  final Duration bulletSpam;
+  static const maxLamaCount = 5;
+  int totalLamaCount = 0;
+  Timer spamTimer;
+  Timer bulletCooldown;
 
-  LamaGame() : super(gravity: Vector2(.0, -50.0), scale: 2.0) {
+  LamaGame({
+    this.spamSpeed = const Duration(milliseconds: 5000),
+    this.bulletSpam = const Duration(milliseconds: 100),
+  }) : super(gravity: Vector2(.0, -50.0), scale: 2.0) {
     addContactCallback(LamaBulletContact());
-    add(createJoystick());
-  }
-
-  JoystickComponent createJoystick() {
-    joystick = JoystickComponent(actions: [
-      JoystickAction(
-        actionId: 1,
-        align: JoystickActionAlign.BOTTOM_LEFT,
-        margin: EdgeInsets.only(bottom: 10, left: 10),
-      ),
-      JoystickAction(
-        actionId: 2,
-        align: JoystickActionAlign.BOTTOM_LEFT,
-        margin: EdgeInsets.only(left: 70, bottom: 10),
-      ),
-    ]);
-    return joystick;
   }
 
   @override
@@ -43,29 +32,53 @@ class LamaGame extends Forge2DGame with MultiTouchDragDetector {
     await images.load('lama.png');
     await images.load('bullet.png');
     await images.load('ground.png');
-    _lama = LamaComponent(
-      SpriteSheet(
-          image: images.fromCache('lama.png'), srcSize: Vector2(24, 36)),
-      Vector2(0, 0),
-    );
-    joystick.addObserver(_lama);
     addAll(createBoundaries(
       viewport,
       Sprite(images.fromCache('ground.png'), srcSize: Vector2(32, 8)),
     ));
-    add(_lama);
+    spamTimer = Timer(
+      spamSpeed.inMilliseconds / Duration.millisecondsPerSecond,
+      callback: () {
+        if (totalLamaCount < maxLamaCount) {
+          final size = viewport.size / viewport.scale;
+          final y = (-size.y / 2) + 20;
+          totalLamaCount++;
+          add(
+            LamaComponent(
+              SpriteSheet(
+                image: images.fromCache('lama.png'),
+                srcSize: Vector2(24, 36),
+              ),
+              Vector2(size.x / 2 - 20, y),
+            ),
+          );
+        }
+      },
+      repeat: true,
+    );
+    bulletCooldown =
+        Timer(bulletSpam.inMilliseconds / Duration.millisecondsPerSecond);
+    spamTimer.start();
   }
 
   @override
-  void onReceiveDrag(DragEvent drag) {
-    joystick.onReceiveDrag(drag);
-    if (joystick.actions.every((b) => !b.isPressed)) {
+  void update(double dt) {
+    super.update(dt);
+    spamTimer.update(dt);
+    if (bulletCooldown.isRunning()) {
+      bulletCooldown.update(dt);
+    }
+  }
+
+  @override
+  void onPanUpdate(DragUpdateDetails details) {
+    if (!bulletCooldown.isRunning()) {
       final bullet = BulletComponent(
         Sprite(images.fromCache('bullet.png')),
-        drag.initialPosition.toVector2(),
+        details.localPosition.toVector2(),
       );
       add(bullet);
+      bulletCooldown.start();
     }
-    super.onReceiveDrag(drag);
   }
 }
