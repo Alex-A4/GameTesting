98a8e9760edc552cae60eb6febc708c3a4418645
= <alex.a4.25@yandex.ru>
2021-02-01 12:25:52 +0300

    Add width to manipulate health width outside

--- lib/components/tower_health_component.dart
+++ lib/components/tower_health_component.dart
@@ -5,18 +5,20 @@ import 'package:flame/components/text_component.dart';
 import 'package:flame/extensions/vector2.dart';
 import 'package:flame/text_config.dart';
 
+/// Component that displays text health of tower.
+/// Health manipulated with [damageTower].
 class TowerHealthComponent extends TextComponent {
   final Vector2 startPosition;
   final double maxHealth;
   Vector2 boxSize;
   double currentHealth;
 
-  TowerHealthComponent(this.maxHealth, this.startPosition)
+  TowerHealthComponent(this.maxHealth, this.startPosition, double width)
       : super(maxHealth.toStringAsFixed(1)) {
-    config = TextConfig(fontSize: 16, color: Color(0xFFFFFFFF));
-    position = startPosition + Vector2(50, 10);
+    config = TextConfig(fontSize: 18, color: Color(0xFFFFFFFF));
+    position = startPosition + Vector2(width / 2, 10);
     anchor = Anchor.center;
-    boxSize = Vector2(100, 20);
+    boxSize = Vector2(width, 20);
     currentHealth = maxHealth;
   }
 
