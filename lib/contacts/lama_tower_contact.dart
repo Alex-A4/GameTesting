07c809e393e1df1e3a78e6820c1d201b599a4ce7
= <alex.a4.25@yandex.ru>
2021-02-02 15:41:32 +0300

    Init lama-tower contact

---
+++ lib/contacts/lama_tower_contact.dart
@@ -0,0 +1,17 @@
+import 'package:flame_forge2d/contact_callbacks.dart';
+import 'package:forge2d/forge2d.dart';
+import 'package:game_testing/components/lama_component.dart';
+import 'package:game_testing/components/tower_component.dart';
+
+class LamaTowerContact extends ContactCallback<LamaComponent, TowerComponent> {
+  @override
+  void begin(LamaComponent a, TowerComponent b, Contact contact) {
+    if (a.canAttack()) {
+      b.damageTower(a.damage);
+      a.makeAttack();
+    }
+  }
+
+  @override
+  void end(LamaComponent a, TowerComponent b, Contact contact) {}
+}
