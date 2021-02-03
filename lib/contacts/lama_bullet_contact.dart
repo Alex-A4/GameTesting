1a0f9bee5c4697a723f1df20ac646b46d142ff9a
= <alex.a4.25@yandex.ru>
2021-02-03 09:00:26 +0300

    Add kick of lama body

--- lib/contacts/lama_bullet_contact.dart
+++ lib/contacts/lama_bullet_contact.dart
@@ -3,13 +3,15 @@ import 'package:forge2d/forge2d.dart';
 import 'package:game_testing/components/bullet_component.dart';
 import 'package:game_testing/components/lama_component.dart';
 
+/// Contact of lama and bullet that damage lama and destroys bullet component
 class LamaBulletContact
     extends ContactCallback<LamaComponent, BulletComponent> {
   @override
   void begin(LamaComponent a, BulletComponent b, Contact contact) {
     if (!a.isDead) {
       b.markToRemove();
-      a.damageLama(b.damage);
+      b.kickBody(a.body);
+      a.damageLama(b.bullet.damage);
     }
   }
 
